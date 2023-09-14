79fec3c3fa5519c654d4feceb31b5568936dec32
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2023-09-14 12:33:46 -0300

    adjust directory

--- registry/new-york/ui/calendar.tsx
+++ registry/new-york/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import * as React from "react"
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
+import * as React from "react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/registry/default/ui/button"
+import { buttonVariants } from "@/registry/new-york/ui/button"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
