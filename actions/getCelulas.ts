d48c14497c3380cce09c3171b3a5cf738bad32fa
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2024-02-16 15:25:09 -0300

    feat fix getCelula

--- actions/getCelulas.ts
+++ actions/getCelulas.ts
@@ -1,20 +1,27 @@
 import { prisma } from "@/lib/prisma";
 
 export const getCelulas = async () => {
-  const res = await prisma.celula.findMany({
-    select: {
-      id: true,
-      name: true,
-      redeId: true,
-      redes:  {
-        select: {
-          id: true,
-          name: true
-        }
+  try {
+    const res = await prisma.celula.findMany({
+      select: {
+        id: true,
+        name: true,
+        redeId: true,
+        redes:  {
+          select: {
+            id: true,
+            name: true
+          }
+        },
+       
       },
-     
-    },
-    orderBy: { name: "asc" },
-  });
-  return res;
+      orderBy: { name: "asc" },
+    });
+    return res;
+    
+  } catch (error) {
+    return ( "Algo deu erra na consulta")
+    
+  }
+  
 };
