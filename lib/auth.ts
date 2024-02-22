32c2ada5ab35446fa6d95999dc2bff46cd6a23c8
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2024-02-22 15:14:46 -0300

    aceito

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+}
